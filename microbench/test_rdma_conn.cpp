d9f50516bea6b7497796d2ff73eb0c89095d9b29
mkuerst <michael.kuerstiner@outlook.com>
2025-02-02 13:43:42 +0100

    remove double def rdtsc()

--- microbench/test_rdma_conn.cpp
+++ microbench/test_rdma_conn.cpp
@@ -8,17 +8,23 @@
 #include <time.h>
 #include <unistd.h>
 #include <vector>
-extern "C" {
 #include <utils.h>
 
-}
+int kReadRatio;
+int kThreadCount;
+int kNodeCount;
+uint64_t kKeySpace = 64 * define::MB;
+double kWarmRatio = 0.8;
+double zipfan = 0;
+int nthreads, client, num_clients,
+num_runs, num_mem_runs,
+mode, duration, nlocks;
+
+DSM *dsm;
 
 int main(int argc, char *argv[]) {
     char *res_file_cum, *res_file_single;
     char *mn_ip, peer_ips[MAX_CLIENTS][MAX_IP_LENGTH];
-    int nthreads, client, num_clients,
-    num_runs, num_mem_runs, use_nodes,
-    scope, mode, duration, nlocks;
 
     parse_cli_args(&nthreads, &num_clients, &nlocks, &client, &duration,
     &mode, &num_runs, &num_mem_runs, &res_file_cum, &res_file_single,
