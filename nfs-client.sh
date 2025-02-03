52aadeb696a0a69230e8c5a5a0c7bf54d4b61bda
mkuerst <michael.kuersteiner@outlook.com>
2025-02-03 04:12:42 -0500

    ..

--- nfs-client.sh
+++ nfs-client.sh
@@ -113,7 +113,7 @@ echo "Mount of $NFSSERVER:$NFSDIR is ready."
 
 
 # sudo chown -R mkuerst:dal-PG0 /nfs/
-sudo bash /local/repository/installLibs.sh
+# sudo bash /local/repository/installLibs.sh
 # sudo sed -i 's/^#PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config 
 # sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config 
 # sudo systemctl restart sshd
