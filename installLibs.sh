9c482e7dd4a9a070c90ee1873a448d825fb825c9
mkuerst <michael.kuerstiner@outlook.com>
2025-02-04 09:11:24 +0100

    dont reinstall everything on reboot - hopefully

--- installLibs.sh
+++ installLibs.sh
@@ -5,75 +5,67 @@ if [ ! -d "tmp" ]; then
 fi
 
 cd tmp
-sudo apt-get -y --force-yes install cmake
-
-# memcached
-sudo apt-get -y --force-yes install memcached libmemcached-dev
-
-# cityhash
-git clone https://github.com/google/cityhash.git
-cd cityhash
-./configure
-make all check CXXFLAGS="-g -O3"
-sudo make install
-cd ..
-
-# boost
-wget https://jaist.dl.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.zip
-unzip boost_1_53_0.zip
-cd boost_1_53_0
-./bootstrap.sh
-# ./b2 install --with-system --with-coroutine --build-type=complete --layout=versioned threading=multi
-./b2 install --with-system --with-coroutine --layout=versioned threading=multi
-sudo apt-get -y --force-yes install libboost-all-dev
-cd ..
-
-# paramiko
-sudo apt-get -y --force-yes install python3-pip
-pip3 install --upgrade pip
-pip3 install paramiko
-
-# gdown
-pip3 install gdown
-pip3 install --upgrade --no-cache-dir gdown
-
-# func_timeout
-pip3 install func_timeout
-
-# matplotlib
-pip3 install matplotlib
 
 # tbb
-git clone https://github.com/wjakob/tbb.git
-cd tbb/build
-cmake ..
-make -j
-sudo make install
-ldconfig
-cd ../..
+# git clone https://github.com/wjakob/tbb.git
+# cd tbb/build
+# cmake ..
+# make -j
+# sudo make install
+# ldconfig
+# cd ../..
 
 # openjdk-8
-sudo apt-get -y --force-yes install openjdk-8-jdk
 
-sudo apt install -y rdma-core librdmacm-dev libibverbs-dev
-sudo apt-get install -y infiniband-diags
-sudo apt install -y libnuma-dev
-sudo apt install -y gh
-sudo apt install -y pdsh 
-sudo apt install -y htop
-sudo apt install -y dsh
-
-cd /local/repository/
 
 OFED_FILE="/local/MLNX_OFED_LINUX-4.9-5.1.0.0-ubuntu20.04-x86_64"
 if [ ! -f "$OFED_FILE" ]; then
     echo "MLNX OFED version is not installed. Running installation script..."
-    if [ -x "$INSTALL_SCRIPT" ]; then
+        sudo apt-get -y --force-yes install cmake
+
+        # memcached
+        sudo apt-get -y --force-yes install memcached libmemcached-dev
+
+        # cityhash
+        git clone https://github.com/google/cityhash.git
+        cd cityhash
+        ./configure
+        make all check CXXFLAGS="-g -O3"
+        sudo make install
+        cd ..
+
+        # boost
+        wget https://jaist.dl.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.zip
+        unzip boost_1_53_0.zip
+        cd boost_1_53_0
+        ./bootstrap.sh
+        # ./b2 install --with-system --with-coroutine --build-type=complete --layout=versioned threading=multi
+        ./b2 install --with-system --with-coroutine --layout=versioned threading=multi
+        sudo apt-get -y --force-yes install libboost-all-dev
+        cd ..
+
+        sudo apt-get -y --force-yes install python3-pip
+        pip3 install --upgrade pip
+        pip3 install paramiko
+
+        pip3 install gdown
+        pip3 install --upgrade --no-cache-dir gdown
+
+        pip3 install func_timeout
+        pip3 install matplotlib
+
+        cd /local/repository/
+        sudo apt-get -y --force-yes install openjdk-8-jdk
+
+        sudo apt install -y rdma-core librdmacm-dev libibverbs-dev
+        sudo apt-get install -y infiniband-diags
+        sudo apt install -y libnuma-dev
+        sudo apt install -y gh
+        sudo apt install -y pdsh 
+        sudo apt install -y htop
+        sudo apt install -y dsh
+
 		sudo bash installMLNX.sh
-    else
-        echo "Error: Install script not found or not executable."
-        exit 1
-    fi
 else
     echo "MLNX OFED version already installed."
 fi
