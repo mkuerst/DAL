35d6366e896f56dec4c59b2cc003dd6cc1c589d3
mkuerst <michael.kuersteiner@outlook.com>
2025-02-05 01:57:10 -0700

    install always ig

--- installLibs.sh
+++ installLibs.sh
@@ -8,67 +8,67 @@ cd tmp
 
 
 
-OFED_FILE="/local/MLNX_OFED_LINUX-4.9-5.1.0.0-ubuntu20.04-x86_64"
-if [ ! -f "$OFED_FILE" ]; then
-    echo "MLNX OFED version is not installed. Running installation script..."
-        sudo apt-get -y --force-yes install cmake
+# OFED_FILE="/local/MLNX_OFED_LINUX-4.9-5.1.0.0-ubuntu20.04-x86_64"
+# if [ ! -f "$OFED_FILE" ]; then
+#     echo "MLNX OFED version is not installed. Running installation script..."
+sudo apt-get -y --force-yes install cmake
 
-        # memcached
-        sudo apt-get -y --force-yes install memcached libmemcached-dev
+# memcached
+sudo apt-get -y --force-yes install memcached libmemcached-dev
 
-        # cityhash
-        git clone https://github.com/google/cityhash.git
-        cd cityhash
-        ./configure
-        make all check CXXFLAGS="-g -O3"
-        sudo make install
-        cd ..
+# cityhash
+git clone https://github.com/google/cityhash.git
+cd cityhash
+./configure
+make all check CXXFLAGS="-g -O3"
+sudo make install
+cd ..
 
-        # boost
-        wget https://jaist.dl.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.zip
-        unzip boost_1_53_0.zip
-        cd boost_1_53_0
-        ./bootstrap.sh
-        # ./b2 install --with-system --with-coroutine --build-type=complete --layout=versioned threading=multi
-        ./b2 install --with-system --with-coroutine --layout=versioned threading=multi
-        sudo apt-get -y --force-yes install libboost-all-dev
-        cd ..
+# boost
+wget https://jaist.dl.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.zip
+unzip boost_1_53_0.zip
+cd boost_1_53_0
+./bootstrap.sh
+# ./b2 install --with-system --with-coroutine --build-type=complete --layout=versioned threading=multi
+./b2 install --with-system --with-coroutine --layout=versioned threading=multi
+sudo apt-get -y --force-yes install libboost-all-dev
+cd ..
 
-        sudo apt-get -y --force-yes install python3-pip
-        pip3 install --upgrade pip
-        pip3 install paramiko
+sudo apt-get -y --force-yes install python3-pip
+pip3 install --upgrade pip
+pip3 install paramiko
 
-        pip3 install gdown
-        pip3 install --upgrade --no-cache-dir gdown
+pip3 install gdown
+pip3 install --upgrade --no-cache-dir gdown
 
-        pip3 install func_timeout
-        pip3 install matplotlib
+pip3 install func_timeout
+pip3 install matplotlib
 
-        tbb
-        git clone https://github.com/wjakob/tbb.git
-        cd tbb/build
-        cmake ..
-        make -j
-        sudo make install
-        ldconfig
-        cd ../..
+tbb
+git clone https://github.com/wjakob/tbb.git
+cd tbb/build
+cmake ..
+make -j
+sudo make install
+ldconfig
+cd ../..
 
-        openjdk-8
-        sudo apt-get -y --force-yes install openjdk-8-jdk
+openjdk-8
+sudo apt-get -y --force-yes install openjdk-8-jdk
 
-        cd /local/repository/
+cd /local/repository/
 
-        sudo apt install -y rdma-core librdmacm-dev libibverbs-dev
-        sudo apt-get install -y infiniband-diags
-        sudo apt install -y libnuma-dev
-        sudo apt install -y gh
-        sudo apt install -y pdsh 
-        sudo apt install -y htop
-        sudo apt install -y dsh
+sudo apt install -y rdma-core librdmacm-dev libibverbs-dev
+sudo apt-get install -y infiniband-diags
+sudo apt install -y libnuma-dev
+sudo apt install -y gh
+sudo apt install -y pdsh 
+sudo apt install -y htop
+sudo apt install -y dsh
 
-		sudo bash installMLNX.sh
-else
-    echo "MLNX OFED version already installed."
-fi
+sudo bash installMLNX.sh
+# else
+#     echo "MLNX OFED version already installed."
+# fi
 
 sudo apt install -y openmpi-bin openmpi-common libopenmpi-dev
