78ebe549b592d89da5b7e0ce44695b84a3ab804f
mkuerst <michael.kuersteiner@outlook.com>
2025-02-25 05:10:51 -0700

    rare failure - datasum x*data_len off

--- microbench/microbench.cpp
+++ microbench/microbench.cpp
@@ -168,13 +168,13 @@ void *mlocks_worker(void *arg) {
     uint64_t range = rlock->getLockNR()-1;
     uint64_t chunk_size = GB(dsmSize) / rlock->getLockNR();
     int sum = 1;
-    // int data_len = page_size / sizeof(uint64_t);
-    int data_len = 1;
+    int data_len = page_size / sizeof(uint64_t);
+    // int data_len = 1;
     uint64_t seed = nodeID*threadNR + id + 42;
     srand(seed);
     ZipfianGenerator zipfian(0.99, range, seed);
-    int num = 0;
-    int cnt = 5;
+    // int num = 0;
+    // int cnt = 5;
 
     pthread_barrier_wait(&global_barrier);
 
